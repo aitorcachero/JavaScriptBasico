b5b1fb97293cdb6817be5c01fe0b4c958b4e3246
Aitor Cachero <aitor.cachero@gmail.com>
2023-01-24 18:47:35 +0100

    Ejercicio 2 - Listas - Arrays

---
+++ Ejercicio2/lista.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Aitor",
+    39,
+    true,
+    new Date(1983, 05, 05),
+    {
+        titulo: "El gran libro de HTML5, CSS3 y JavaScript",
+        autor: "Juan Diego Gauchat",
+        fecha: new Date(2019, 03, 12),
+        url: "https://amzn.eu/d/bJ5Humt",
+    }
+    ]
+
