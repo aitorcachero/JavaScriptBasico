4ee45f96713882aa209feb3ebcf4088c74e0b2de
Aitor Cachero <aitor.cachero@gmail.com>
2023-01-25 17:55:22 +0100

    Ejercicio 5 - Números

---
+++ Ejercicio5/Ejercicio5.js
@@ -0,0 +1,19 @@
+//Una variable que contenga tu altura en centímetros (entero)
+let alturaint = 174
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+let alturafloat = 1.74
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesofloat = 71.3
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaceil = Math.ceil(alturafloat)
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesofloor = Math.floor(pesofloat)
+
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+
