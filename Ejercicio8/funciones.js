5bddc4d65aaaa765af55cc3c822e35ba93110e71
Aitor Cachero <aitor.cachero@gmail.com>
2023-01-30 14:55:28 +0100

    Ejercicio 8 - Funciones

---
+++ Ejercicio8/funciones.js
@@ -0,0 +1,22 @@
+// - Una función sin parámetros que devuelva siempre "true"
+function vacia() {
+     return true
+}
+
+
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+
+async function timeOutPromise() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+
+
+// - Una función generadora de índices pares automáticos
+
+function* generandoPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
