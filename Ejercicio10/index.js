20bb5ee84a596b42d36f74025d3083ee5ac7444d
Aitor Cachero <aitor.cachero@gmail.com>
2023-02-06 11:37:23 +0100

    Ejercicio 10 - Módulos en JavaScript

---
+++ Ejercicio10/index.js
@@ -0,0 +1,6 @@
+import * as moduloMath from './modulos/controller.js';
+import chalk from 'chalk';
+
+console.log(moduloMath.suma(1, 2));
+
+console.log(chalk.green(moduloMath.multiplica(4, 5)));
--- Ejercicio7/fechas.js
+++ Ejercicio7/fechas.js
@@ -1,14 +1,55 @@
 // - La fecha de hoy
-fechaHoy = new Date()
+fechaHoy = new Date();
 
 // - La fecha de tu nacimiento
-fechaNacimiento = new Date(1983, 05, 05)
+fechaNacimiento = new Date(1983, 05, 05);
 
 // - Una variable que contenga el día de tu nacimiento
-const dia = fechaNacimiento.getDate()
+const dia = fechaNacimiento.getDate();
 
 // - Una variable que contenga el mes de tu nacimiento (recuerda que Enero es mes 0)
-const mes = fechaNacimiento.getMonth() +1
+let mes = fechaNacimiento.getMonth() + 1;
 
 // - Una variable que contenga el año de tu nacimiento (con 4 dígitos)
-const year = fechaNacimiento.getFullYear()
+const year = fechaNacimiento.getFullYear();
+
+switch (mes) {
+  case 1:
+    mes = 'Enero';
+    break;
+  case 2:
+    mes = 'Febrero';
+    break;
+  case 3:
+    mes = 'Marzo';
+    break;
+  case 4:
+    mes = 'Abril';
+    break;
+  case 5:
+    mes = 'Mayo';
+    break;
+  case 6:
+    mes = 'Junio';
+    break;
+  case 7:
+    mes = 'Julio';
+    break;
+  case 8:
+    mes = 'Agosto';
+    break;
+  case 9:
+    mes = 'Septiembre';
+    break;
+  case 10:
+    mes = 'Octubre';
+    break;
+  case 11:
+    mes = 'Noviembre';
+    break;
+  case 12:
+    mes = 'Diciembre';
+    break;
+}
+
+console.log(dia + ' de ' + mes + ' de ' + year);
